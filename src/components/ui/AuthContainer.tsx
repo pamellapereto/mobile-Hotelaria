98d226018129b0f4529b39c27da8e62bb9516e49
PAMELLA CHRISTINI PERETO E SILVA <pamella.cpesilva@senacsp.edu.br>
2026-02-24 16:01:09 -0300

    feat: authcontext com requisicao para login

--- src/components/ui/AuthContainer.tsx
+++ src/components/ui/AuthContainer.tsx
@@ -11,7 +11,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { global } from "./styles";
 
 type Props = {
-  title?: string;   
+  title?: string;
   subtitle?: string;
   icon?: keyof typeof FontAwesome6.glyphMap;
   children: React.ReactNode;
@@ -26,13 +26,12 @@ const AuthContainer = ({ title, subtitle, icon, children }: Props) => {
       >
         <ScrollView contentContainerStyle={global.container}>
           <View style={global.header}>
-
             {!!icon && <FontAwesome6 name={icon} size={30} color="purple" />}
-            {!!title && <Text style={global.title}>{title}</Text>}  
+            {!!title && <Text style={global.title}>{title}</Text>}
             {!!subtitle && <Text style={global.subtitle}>{subtitle}</Text>}
-
           </View>
-          <View>{children}</View> {/* Retirada a estilização global.content para não impactar no 
+          <View>{children}</View>{" "}
+          {/* Retirada a estilização global.content para não impactar no 
           componente AuthContainer a ser chamado em outras telas */}
         </ScrollView>
       </KeyboardAvoidingView>
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,15 +1,60 @@
-import { createContext } from "react";
+import { API_URL } from "@/constants/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useEffect, useMemo, useState } from "react";
 
 type AuthContextProps = {
   token: string | null;
   isLoading: boolean;
-  signIn: (email: string, password: string) => Promise<void>;
+  signIn: (email: string, senha: string) => Promise<void>;
   signOut: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Carregar token ao abrir o app
+  useEffect(() => {
+    (async () => {
+      try {
+        const stored = await AsyncStorage.getItem("token");
+        if (stored) setToken(stored);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  async function signIn(email: string, senha: string) {
+    const res = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+    // Back-end retorna erro {erro: "..."} quando falha
+    if (!res.ok) {
+      const err = await res.json().catch(() => null);
+      throw new Error(err?.erro || "Credenciais inválidas");
+    }
+    //Back-end retorna JSON
+    const tokenAPI: string = await res.json();
+
+    await AsyncStorage.setItem("token", tokenAPI);
+    setToken(tokenAPI);
+  }
+
+  //SignOut:
+  async function signOut() {}
+
+  const value = useMemo(
+    () => ({ token, isLoading, signIn, signOut }),
+    [token, isLoading],
+  );
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
